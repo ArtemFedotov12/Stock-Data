ba6ac3a880402b6e3098da5c43f65d369c85c653
Artem Fedotov <a.fedotov@andersenlab.com>
2020-04-02 21:23:36 +0300

    init commit

---
+++ src/main/java/com/example/stockdata/model/AbstractRemovableEntity.java
@@ -0,0 +1,19 @@
+package com.example.stockdata.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+// 'callSuper = false' --- means dont't take into consideration parent fields
+@EqualsAndHashCode(callSuper = false)
+@Data
+@MappedSuperclass
+public abstract class AbstractRemovableEntity extends AbstractEntity {
+
+    @Column(name = "removal_date")
+    private LocalDateTime removalDate;
+
+}
